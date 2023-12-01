70ad4cbb9ce8ad24f87d37677e81d6c3200fd586
fmeleard84 <fmeleard@gmail.com>
2023-12-01 14:38:26 +0100

    requirement

--- helloworld/main.py
+++ helloworld/main.py
@@ -1,7 +1,17 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, url_for
+import os
+
+import connexion
 
 app = Flask(__name__)
+
+# Lecture du fichier swagger.yml pour définir les points d'arrivée (endpoints)
+app.add_api('swagger.yml')
+
+@app.route('/audio/montage/<filename>')
+def download_file(filename):
+    return send_from_directory("montage", filename)
 
 @app.route("/")
 def home_view():
